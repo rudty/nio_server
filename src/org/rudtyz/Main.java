1f782335d32633e98b0827dced2741f266235984
석경우 <rudtyz@gmail.com>
2020-09-27 18:14:57 +0900

    기본 예외처리 추가

--- src/org/rudtyz/Main.java
+++ src/org/rudtyz/Main.java
@@ -1,6 +1,7 @@
 package org.rudtyz;
 
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
@@ -10,51 +11,111 @@ import java.nio.channels.SocketChannel;
 
 public class Main {
 
-    public static void doAccept(SelectionKey key) throws Exception {
+    public static void doAccept(SelectionKey key) {
         var serverSocketChannel = (ServerSocketChannel) key.channel();
-        var client = serverSocketChannel.accept();
-        client.configureBlocking(false);
-        client.register(Poll.selector, SelectionKey.OP_READ);
+        SocketChannel client = null;
+        try {
+            client = serverSocketChannel.accept();
+            client.configureBlocking(false);
+            client.register(Poll.selector, SelectionKey.OP_READ);
+        } catch (IOException e) {
+            System.out.println("client connect fail");
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException ignore) {
+                }
+            }
+        }
     }
 
-    public static void doWrite(SelectionKey key) throws Exception {
+    public static void registerRead(SocketChannel client) {
+        if (client.isConnected()) {
+            try {
+                client.register(Poll.selector, SelectionKey.OP_READ);
+            } catch (ClosedChannelException ignore) {
+            }
+        }
+    }
+
+    public static void doWrite(SelectionKey key) {
         var client = (SocketChannel) key.channel();
 
         var buf = (ReceiveBuffer) key.attachment();
         var sendBuffer = buf.toByteBuffer();
-        client.write(sendBuffer);
+        try {
+            client.write(sendBuffer);
+        } catch (IOException e) {
+            if (!client.isConnected()) {
+                return;
+            }
+            System.out.println("write fail");
+        }
 
-        client.register(Poll.selector, SelectionKey.OP_READ);
+        registerRead(client);
     }
 
-    public static void doRead(SelectionKey key) throws Exception {
+    public static void doRead(SelectionKey key) {
         SocketChannel client = (SocketChannel) key.channel();
 
         ByteBuffer buf = ByteBuffer.allocate(1024);
-        int receiveSize = client.read(buf);
+        try {
+            int receiveSize = client.read(buf);
 
-        if (receiveSize <= 0) {
-            key.cancel();
-            client.close();
-            return;
+            if (receiveSize <= 0) {
+                key.cancel();
+                try {
+                    client.close();
+                } catch (IOException ignore) {
+                }
+                return;
+            }
+
+            registerRead(client);
+
+            onMessage(client, buf.array(), receiveSize);
+        } catch (IOException ignore) {
         }
-
-
-        onMessage(client, buf.array(), receiveSize);
     }
 
 
-    public static void write(SocketChannel client, ReceiveBuffer b) throws ClosedChannelException {
-        client.register(
-                Poll.selector,
-                SelectionKey.OP_READ | SelectionKey.OP_WRITE,
-                b);
+    public static void write(SocketChannel client, ReceiveBuffer b) {
+        if (client.isConnected()) {
+            try {
+                client.register(
+                        Poll.selector,
+                        SelectionKey.OP_READ | SelectionKey.OP_WRITE,
+                        b);
+            } catch (ClosedChannelException ignore) {
+            }
+        }
     }
 
 
-    public static void onMessage(SocketChannel client, byte[] b, int len) throws Exception{
+    public static void onMessage(SocketChannel client, byte[] b, int len) {
         System.out.println(new String(b, 0, len));
         write(client, new ReceiveBuffer(b, len));
+    }
+
+    public static void netPoll() {
+        var keys = Poll.selector.selectedKeys();
+
+        for (var key : keys) {
+            if (!key.isValid()) {
+                continue;
+            }
+
+            if (key.isAcceptable()) {
+                doAccept(key);
+            }
+            if (key.isWritable()) {
+                doWrite(key);
+            }
+            if (key.isReadable()) {
+                doRead(key);
+            }
+        }
+        keys.clear();
     }
 
     public static void main(String[] args) throws Exception {
@@ -65,22 +126,7 @@ public class Main {
 
         while (true) {
             if (Poll.selector.select(10L) > 0) {
-                var keys = Poll.selector.selectedKeys();
 
-                for (var key : keys) {
-                    if (!key.isValid()) {
-                        continue;
-                    }
-
-                    if (key.isAcceptable()) {
-                        doAccept(key);
-                    } else if (key.isWritable()) {
-                        doWrite(key);
-                    } else if (key.isReadable()) {
-                        doRead(key);
-                    }
-                }
-                keys.clear();
             }
         }
     }
